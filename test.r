725571b8d6e16fe1b80e9d129c36795672842a0c
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-03-28 13:54:32 +0200

    Update test.r

--- test.r
+++ test.r
@@ -4,26 +4,27 @@ library(tidyverse)
 library(lubridate)
 
 tests <- tribble(~Zeit, ~Art,
-                 "5/12/2020 15.40", "Antigen", 
-                 "23/12/2020 16.31", "Antigen", 
-                 "9/1/2021 10.33", "Antigen", 
-                 "17/1/2021 9.18", "Antigen", 
-                 "23/1/2021 9.20", "Antigen",
-                 "30/1/2021 9.25", "Antigen",
-                 "7/2/2021 15.59", "Antigen",
-                 "13/2/2021 9.48", "Antigen",
-                 "20/2/2021 9.55", "Antigen",
-                 "25/2/2021 18.04", "Antigen",
-                 "27/2/2021 9.57", "Antigen",
-                 "6/3/2021 9.44", "Antigen",
-                 "12/3/2021 16.26", "Antigen",
+                 "5/12/2020 15.40", "Ag", 
+                 "23/12/2020 16.31", "Ag", 
+                 "9/1/2021 10.33", "Ag", 
+                 "17/1/2021 9.18", "Ag", 
+                 "23/1/2021 9.20", "Ag",
+                 "30/1/2021 9.25", "Ag",
+                 "7/2/2021 15.59", "Ag",
+                 "13/2/2021 9.48", "Ag",
+                 "20/2/2021 9.55", "Ag",
+                 "25/2/2021 18.04", "Ag",
+                 "27/2/2021 9.57", "Ag",
+                 "6/3/2021 9.44", "Ag",
+                 "12/3/2021 16.26", "Ag",
                  "19/3/2021 4.46", "PCR",
-                 "20/3/2021 9.48", "Antigen",
-                 "21/3/2021 18.08", "Antigen",
+                 "20/3/2021 9.48", "Ag",
+                 "21/3/2021 18.08", "Ag",
                  "23/3/2021 4.58", "PCR",
-                 "25/3/2021 5.01", "PCR") %>%
+                 "25/3/2021 5.01", "PCR",
+                 "28/3/2021 9.20", "Ag") %>%
   mutate(Zeit = dmy_hm(Zeit, tz = "Europe/Vienna"),
-         Dauer = case_when(Art == "Antigen" ~ 48,
+         Dauer = case_when(Art == "Ag" ~ 48,
                            TRUE ~ 72),
          Bis = Zeit + lubridate::hours(Dauer)) %>%
   rownames_to_column(., var = "He") %>%
@@ -37,7 +38,7 @@ ggplot(data = tests) +
   scale_x_datetime(name = "Zeit [-90d ... jetzt ... +5d]", limits = c(now() - days(90), now() + days(5)), expand = c(.01, .01)) + 
   scale_y_continuous(name = "'Schutz'", breaks = c(0, 1), minor_breaks = NULL, expand = c(.015, .015)) +
   scale_fill_manual(name = "Testart", values = set_names(x = RColorBrewer::brewer.pal(3, "YlGn")[-1],
-                                                         nm = c("Antigen", "PCR"))) +
+                                                         nm = c("Ag", "PCR"))) +
   labs(title = "COVID-19 Tests", subtitle = "Thomas") +
   theme_minimal()-> p
 
