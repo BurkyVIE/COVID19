1a47c2054f5ee26cb6fcf6f9e86bfcb747d05b9c
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-04-05 21:53:05 +0200

    Update tests.r

--- tests.r
+++ tests.r
@@ -1,39 +1,46 @@
-# Tracking Thomas' COVID19 test
+# Tracking Thomas' COVID19 tests
 
 library(tidyverse)
 library(lubridate)
 
-tests <- tribble(~Zeit, ~Art, ~Befund,
-                 "5/12/2020 15.40", "Ag", NA_character_,
-                 "23/12/2020 16.31", "Ag", NA_character_, 
-                 "9/1/2021 10.33", "Ag", NA_character_,
-                 "17/1/2021 9.18", "Ag", NA_character_,
-                 "23/1/2021 9.20", "Ag", NA_character_,
-                 "30/1/2021 9.25", "Ag", NA_character_,
-                 "7/2/2021 15.59", "Ag", NA_character_,
-                 "13/2/2021 9.48", "Ag", NA_character_,
-                 "20/2/2021 9.55", "Ag", NA_character_,
-                 "25/2/2021 18.04", "Ag", NA_character_,
-                 "27/2/2021 9.57", "Ag", NA_character_,
-                 "6/3/2021 9.44", "Ag", NA_character_,
-                 "12/3/2021 16.26", "Ag", NA_character_,
-                 "18/3/2021 4.46", "PCR", "Befunde/0318_L21031800195.pdf",
-                 "20/3/2021 9.48", "Ag", "Befunde/0320_U07.1-2117023-2021_AGLabCluster_800010648264_1_1000000913223840693451__.pdf",
-                 "21/3/2021 18.08", "Ag", "Befunde/0321_U07.1-2117023-2021_AGLabCluster_800010702190_1_1000000931823120263007__.pdf",
-                 "23/3/2021 4.58", "PCR", "Befunde/0323_L21032300178.pdf",
-                 "25/3/2021 5.01", "PCR", "Befunde/0325_L21032500256.pdf",
-                 "28/3/2021 9.20", "Ag", "Befunde/0328_U07.1-2117023-2021_AGLabCluster_800010817875_1_1000001052716230980622__.pdf",
-                 "29/3/2021 7.42", "PCR", "Befunde/0329_da08e473-a55b-4d5e-be10-8b20bef6c653.pdf",
-                 "1/4/2021 6.21", "PCR", "Befunde/0401_d2674b08-5c33-4313-8737-78d2d4e43945.pdf",
-                 "3/4/2021 9.38", "Ag", "Befunde/0403_U07.1-2517090-2021_AGLabCluster_800010921161_1_1000001191178180040093__.pdf",
-                 "5/4/2021 9.03", "Ag", "Befunde/0405_U07.1-2517090-2021_AGLabCluster_800011000503_1_1000001217569280666552__.pdf") %>%
+tests <- tribble(~Zeit, ~Art,
+                 "5/12/2020 15.40", "Ag",
+                 "23/12/2020 16.31", "Ag", 
+                 "9/1/2021 10.33", "Ag",
+                 "17/1/2021 9.18", "Ag",
+                 "23/1/2021 9.20", "Ag",
+                 "30/1/2021 9.25", "Ag",
+                 "7/2/2021 15.59", "Ag",
+                 "13/2/2021 9.48", "Ag",
+                 "20/2/2021 9.55", "Ag",
+                 "25/2/2021 18.04", "Ag",
+                 "27/2/2021 9.57", "Ag",
+                 "6/3/2021 9.44", "Ag",
+                 "12/3/2021 16.26", "Ag",
+                 "18/3/2021 4.46", "PCR",
+                 "20/3/2021 9.48", "Ag",
+                 "21/3/2021 18.08", "Ag",
+                 "23/3/2021 4.58", "PCR",
+                 "25/3/2021 5.01", "PCR",
+                 "28/3/2021 9.20", "Ag",
+                 "29/3/2021 7.42", "PCR",
+                 "1/4/2021 6.21", "PCR",
+                 "3/4/2021 9.38", "Ag",
+                 "5/4/2021 9.03", "Ag") %>% 
   mutate(Zeit = dmy_hm(Zeit, tz = "Europe/Vienna"),
          Dauer = case_when(Art == "Ag" ~ 48,
                            TRUE ~ 72),
          Ende = Zeit + lubridate::hours(Dauer)) %>%
   rownames_to_column(., var = "Lfnr") %>%
-  mutate(He = ((as.numeric(Lfnr) - 1) %% 3 + 9) * .05) %>% 
-  relocate(Befund, .after = Ende)
+  mutate(Horizont = ((as.numeric(Lfnr) - 1) %% 3 + 9) * .05,
+         Key = strftime(Zeit, "%y%m%d"))
+
+# Verfügbare Befunde zuordnen
+tests <- left_join(tests,
+                   tibble(Befund = dir("Befunde/")) %>%
+                     mutate(Key = str_sub(Befund, 1, 6),
+                     Befund = paste0("Befunde/", Befund)),
+                   by = "Key") 
 
 # Zeitraum fuer Darstellung (in Tagen)
 range <- c(-50, 0, 5)
@@ -59,7 +66,7 @@ ggplot(data = testungen) +
     sigma = 2.5) +
   geom_vline(xintercept = frame[2], linetype = "dotted", size = 1, color = "orangered") +
   ggfx::with_shadow(
-    geom_errorbarh(mapping = aes(y = He, xmin = Zeit, xmax = Ende), height = .02, size = 1.1, color = "steelblue"),
+    geom_errorbarh(mapping = aes(y = Horizont, xmin = Zeit, xmax = Ende), height = .02, size = 1.1, color = "steelblue"),
     color = "steelblue", x_offset = 0, y_offset = 4, sigma = 2.5) +
   ggfx::with_shadow(
     geom_errorbarh(data = zeitraeume, mapping = aes(y = .15, xmin = Von, xmax = Ende), height = .03, size = 1.5, color = "royalblue"),
