e04ba1c20c5060e7899bee717704ad8c0ae91b1a
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-11-11 05:24:37 +0100

    Update tests.r

--- tests.r
+++ tests.r
@@ -36,7 +36,7 @@ tests <- tibble(
            "8/9/2021/10/21 PCR LH",   "11/9/2021/7/48 PCR LH",  "18/9/2021/7/0 PCR LH",   "20/9/2021/7/14 PCR LH",  # 87
            "22/9/2021/6/50 PCR LH",   "24/9/2021/8/35 PCR LH",  "5/10/2021/5/10 PCR LH",  "9/10/2021/7/56 PCR LH",  # 91
            "15/10/2021/10/40 PCR LH", "19/10/2021/11/2 PCR LH", "25/10/2021/6/43 PCR LH", "31/10/2021/7/39 PCR LH", # 95
-		   "6/11/2021/8/17 PCR LH")) %>% 
+		   "6/11/2021/8/17 PCR LH",   "8/11/2021/11/52/PCR LH")) %>% 
   separate(Data, into = c("Zeit", "Art", "Anbieter"), sep = " ") %>% 
   rownames_to_column(., var = "Lfnr") %>% # Zeilennummern - spaeter Ableitung der y-Koordinate im Plot (Range-Bars)
   mutate(Lfnr = as.numeric(Lfnr),
